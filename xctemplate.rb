1f0448c0bff65ba1056eaf0b5d3906c5824790b4
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2020-05-03 13:41:09 +0200

    Bump xctemplate to 0.0.3

--- xctemplate.rb
+++ xctemplate.rb
@@ -2,7 +2,7 @@
 class Xctemplate < Formula
   desc "Xcode template manager"
   homepage "https://github.com/gaetanzanella/XCTemplateInstaller"
-  url "https://github.com/gaetanzanella/XCTemplateInstaller.git", :tag => "0.0.2", :revision => "d072cc9c3cabc01426e9c6e215b5e4a4ffea6573"
+  url "https://github.com/gaetanzanella/XCTemplateInstaller.git", :tag => "0.0.3"
   head "https://github.com/gaetanzanella/XCTemplateInstaller.git", :shallow => false
 
   depends_on "cmake" => :build
