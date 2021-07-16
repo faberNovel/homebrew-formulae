cc62a902c377e099559f306e92c5be560a5fda1f
Bot-Fabernovel <67961859+Bot-Fabernovel@users.noreply.github.com>
2021-07-16 14:57:22 +0200

    Update xcresource-cli to 0.1.1

--- Formula/xcresource.rb
+++ Formula/xcresource.rb
@@ -1,15 +1,14 @@
-
 class Xcresource < Formula
   desc "Xcode resources manager"
   homepage "https://github.com/faberNovel/xcresource-cli"
-  url "https://github.com/faberNovel/xcresource-cli.git", :tag => "0.1.0"
+  url "https://github.com/faberNovel/xcresource-cli.git", tag: "0.1.1", revision: "f7a85b85de9c7690563bf670cced9df841d250ad"
   head "https://github.com/faberNovel/xcresource-cli.git", :shallow => false
 
   depends_on "cmake" => :build
   depends_on :xcode => ["12", :build]
 
   def install
-      system "make", "install", "prefix=#{prefix}"
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
