c87123bb00b3508458346ba2eb2bc0bb5ea05eb2
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2021-07-16 14:11:51 +0200

    Fix Xcode dependency

--- Formula/xcresource.rb
+++ Formula/xcresource.rb
@@ -2,11 +2,11 @@
 class Xcresource < Formula
   desc "Xcode resources manager"
   homepage "https://github.com/faberNovel/xcresource-cli"
-  url "https://github.com/faberNovel/xcresource-cli.git", :tag => "0.0.8"
+  url "https://github.com/faberNovel/xcresource-cli.git", :tag => "0.1.0"
   head "https://github.com/faberNovel/xcresource-cli.git", :shallow => false
 
   depends_on "cmake" => :build
-  depends_on :xcode => ["11.4", :build]
+  depends_on :xcode => ["12", :build]
 
   def install
       system "make", "install", "prefix=#{prefix}"
