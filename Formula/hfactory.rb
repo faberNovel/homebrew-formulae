d401a74553e68c8f40cceda374e8862d3610d7e9
Bot-Fabernovel <67961859+Bot-Fabernovel@users.noreply.github.com>
2023-11-29 09:04:26 +0100

    hfactory 1.9.2

--- Formula/hfactory.rb
+++ Formula/hfactory.rb
@@ -2,8 +2,8 @@ class Hfactory < Formula
   desc "A useful cli for factory developers"
   homepage "https://github.com/faberNovel/hfactory-cli"
   url "git@github.com:faberNovel/hfactory-cli.git",
-  tag: "1.9.1",
-  version: "1.9.1", :using => :git
+  tag: "1.9.2",
+  version: "1.9.2", :using => :git
 
   depends_on "fzf"
   depends_on "bat"
